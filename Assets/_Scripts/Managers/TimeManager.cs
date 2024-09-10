62e08c92106891093faaf99ad9ac112382c1f78e
hieult <hieult@falcongames.com>
2024-09-10 18:26:11 +0700

    Time and event

--- Assets/_Scripts/Managers/TimeManager.cs
+++ Assets/_Scripts/Managers/TimeManager.cs
@@ -1,18 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NaughtyAttributes;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TimeManager : MonoBehaviour
 {
-    [ReadOnly] public int CurrentWeek = 0;
+    [ReadOnly] public int CurrentWeek = 1;
     [ReadOnly] public int CurrentDay = 1;
 
     [SerializeField] private float _secondsPerGameDay;
+    [SerializeField] private List<EventOfWeek> _eventWeeks;
 
     private float _timer;
 
+    private void Start()
+    {
+        _eventWeeks = _eventWeeks.OrderBy(e => e.Week).ToList();
+    }
+
     private void Update()
     {
+        if (CurrentWeek == _eventWeeks[0].Week)
+        {
+            _eventWeeks[0].CallStorm();
+            Instantiate(_eventWeeks[0].EnemyWave,GameManager.Instance.UnitManager.transform);
+            
+            _eventWeeks.RemoveAt(0);
+        }
+        
         _timer += Time.deltaTime;
         if (_timer >= _secondsPerGameDay)
         {
@@ -25,5 +43,19 @@ public class TimeManager : MonoBehaviour
                 CurrentWeek++;
             }
         }
+    }
+    
+}
+
+[Serializable]
+public class EventOfWeek
+{
+    public int Week;
+    public string Storm;
+    public GameObject EnemyWave;
+
+    public void CallStorm()
+    {
+        throw new NotImplementedException();
     }
 }
