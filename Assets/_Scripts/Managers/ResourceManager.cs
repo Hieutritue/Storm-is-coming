4ceebe7c7f5e1e6a5c042dd430c729c30bb6af73
NobleMode <laminhcokho@gmail.com>
2024-09-13 16:15:50 +0700

    Update

--- Assets/_Scripts/Managers/ResourceManager.cs
+++ Assets/_Scripts/Managers/ResourceManager.cs
@@ -1,15 +1,28 @@
 using NaughtyAttributes;
 using UnityEngine;
 
-public class ResourceManager : BaselineManager
+public class ResourceManager : MonoBehaviour
 {
     public int Meat;
     public int Wood;
     public int Iron;
     public int Gold;
 
-    public void Start()
+    public bool CheckEnoughResources(TileRequirement requirements)
     {
-        resourceManager = this;
+        if (Meat >= requirements.meat && Wood >= requirements.wood && Iron >= requirements.iron && Gold >= requirements.gold)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void RemoveResources(int meat, int wood, int iron, int gold)
+    {
+        Meat -= meat;
+        Wood -= wood;
+        Iron -= iron;
+        Gold -= gold;
     }
 }
